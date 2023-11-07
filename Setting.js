ea6b7f8c7263e32958fb093a972ed2badad262a7
zetauh <148994189+zetauh@users.noreply.github.com>
2023-11-07 10:48:35 +0530

    Update Setting.js

--- Setting.js
+++ Setting.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════,[Make sure you put all values in "" , '']\\
-global.owner = process.env.OWNER_NUMBER || '923466319114' ; // Add Your Number without +
+global.owner = process.env.OWNER_NUMBER || '919747257996' ; // Add Your Number without +
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Maher-Zubair:SIGMA-MD@zubi.9g6b16y.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp" ; // put mongodb key here
 global.port=5000  ;
 global.audio = '' ; 
@@ -11,49 +11,49 @@ global.video = '' ;
 global.blockJids = process.env.BLOCK_JID ||'120363169665426586@g.us' ;
 global.allowJids = process.env.ALLOW_JID ||'120363169665426586@g.us' ;
 global.email = 'HELP_SIGMA-MD@outlook.com' ; 
-global.github = 'https://github.com/Maher-Zubair/SIGMA-MD' ;
+global.github = 'https://github.com/zetauh/ZETA-XD' ;
 global.location = 'Lahore Pakistan' ;
 global.timezone  = process.env.TIME_ZONE || 'Asia/Karachi' //add correct timezone or leave it same , otherwise you get erros
 global.gurl = 'https://www.youtube.com/@InnoxentTech?sub_confirmation=1' ; 
-global.sudo =  process.env.SUDO || "923466319114" ; // Do not change it
-global.devs = "923466319114"; //Dont change it From here
-global.mztit = process.env.MZTIT ||"🅼♥︎❚❚♥︎🆉", // add your title here
-global.Gname = process.env.GNAME ||"sɪɢᴍᴀ ᴹᴰ-sᴜᴘᴘᴏʀᴛ",
+global.sudo =  process.env.SUDO || "919747257996" ; // Do not change it
+global.devs = "919747257996"; //Dont change it From here
+global.mztit = process.env.MZTIT ||"𝐙𝐄𝐓𝐀", // add your title here
+global.Gname = process.env.GNAME ||"ᴢᴇᴛᴀ ᴹᴰ-sᴜᴘᴘᴏʀᴛ",
 global.zyt = process.env.ZYT || 'https://www.youtube.com/@InnoxentTech?sub_confirmation=1',
-global.waUrl = process.env.WAURL ||"https://chat.whatsapp.com/CmY0THcJCUYEGxLJulhcRV",
+global.waUrl = process.env.WAURL ||"https://chat.whatsapp.com/JIfqwty3eZLHePi6VaxvXd",
 global.website = 'http://lnkiy.in/SIGMA-MD-WEB' ; 
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/03e49e6e2057568db8926.jpg' ;
 module.exports = {
   sessionName: process.env.SESSION_ID || '',      //Put Your Session Id Here
-  botname: process.env.BOT_NAME || 'sɪɢᴍᴀ ᴹᴰ',  // add the botname you want
-  ownername:  process.env.OWNER_NAME || `ᴍᴀʜᴇʀ ᴢᴜʙᴀɪʀ`, // add your name
+  botname: process.env.BOT_NAME || 'ᴢᴇᴛᴀ ᴹᴰ',  // add the botname you want
+  ownername:  process.env.OWNER_NAME || `ʙᴀᴅᴀɴ ꜱᴇʀ`, // add your name
   author:  process.env.PACK_AUTHER || 'ᴍᴀʜᴇʀ ᴢᴜʙᴀɪʀ', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false', // if it true it automatically views status and send status in your inbox
-  packname:  process.env.PACK_NAME || "sɪɢᴍᴀ ᴹᴰ" , 
-  autoreaction: process.env.AUTO_REACTION || 'true', // if it true it will react to all messages
+  packname:  process.env.PACK_NAME || "ᴢᴇᴛᴀ ᴹᴰ" , 
+  autoreaction: process.env.AUTO_REACTION || 'false', // if it true it will react to all messages
   antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
-  alwaysonline: process.env.ALWAYS_ONLINE || 'true', // it will show always online, false it if you dont want
+  alwaysonline: process.env.ALWAYS_ONLINE || 'false', // it will show always online, false it if you dont want
   antifake :   process.env.FAKE_COUNTRY_CODE ||'212',
-  readmessage: process.env.READ_MESSAGE || 'true', // it will read all your messages, false it if you dont want
+  readmessage: process.env.READ_MESSAGE || 'false', // it will read all your messages, false it if you dont want
   HANDLERS: process.env.PREFIX || '.',
-  warncount : process.env.WARN_COUNT || 2,
+  warncount : process.env.WARN_COUNT || 4,
   disablepm: process.env.DISABLE_PM || "false", // if true it will disable your pm
   MsgsInLog:process.env.MSGS_IN_LOG ||'false',
   pmMsgsInLog:process.env.PM_MSGS_IN_LOGS ||'false',
-  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'true', 
+  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false', 
   antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
   antilinkaction: process.env.BRANCH || 'remove',
   BRANCH: process.env.BRANCH || 'Main',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "34wcCGPoe3yaGtpiBZgx4SN7", // add your remove bg key if you have it
-  caption :process.env.CAPTION || "\t*⤹★ᴘᴏᴡᴇʀᴇᴅ ʙʏ★⤸ sɪɢᴍᴀ ᴹᴰ* ",   //*『sᴜʙsᴄʀɪʙᴇ • ɪɴɴᴏxᴇɴᴛ ᴛᴇᴄʜ』https://www.youtube.com/@InnoxentTech,
+  caption :process.env.CAPTION || "\t*⤹★ᴘᴏᴡᴇʀᴇᴅ ʙʏ★⤸ ᴢᴇᴛᴀ ᴹᴰ* ",   //*『sᴜʙsᴄʀɪʙᴇ • ɪɴɴᴏxᴇɴᴛ ᴛᴇᴄʜ』https://www.youtube.com/@InnoxentTech,
   promote_demote_messages : process.env.PROMOTE_DEMOTE_MESSAGES || 'true' ,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY ||'' , // add your openai key if you have it
   VERSION: process.env.VERSION || '𝚅.𝟷.𝟸.𝟽',
-  LANG: process.env.THEME|| 'SIGMA_MD',
+  LANG: process.env.THEME|| 'GOJO',
   menu : process.env.MENU || '', // Not Available in current Version
-  WORKTYPE: process.env.WORKTYPE || 'public' // if private only you can use your bot, if public everyone use your bot
+  WORKTYPE: process.env.WORKTYPE || 'private' // if private only you can use your bot, if public everyone use your bot
 };
 
 
